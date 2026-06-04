3c56e3ca0ddb245089e444d8e02b9c2621753b75
Vittorio Aluigi <163413387+VitAluigi@users.noreply.github.com>
2026-06-04 16:37:40 +0200

    Change page title to 'SPINGERE CAVALLO'

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from modules.analisi import (scopri_analisi, kpi_portafoglio,
                               unisci_ship_patrimoniale, unisci_ship_economico)
 from main import leggi_portafoglio, calcola_analisi, genera_output, _merge_ptf_eco
 
-st.set_page_config(page_title="Report Portafoglio Titoli", layout="wide")
+st.set_page_config(page_title="SPINGERE CAVALLO", layout="wide")
 
 st.markdown("""
 <style>
